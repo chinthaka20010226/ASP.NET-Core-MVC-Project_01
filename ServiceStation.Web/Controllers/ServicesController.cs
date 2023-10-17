cfd98b3f0b159d76e556a142c40990272bd7718f
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-10-17 16:28:01 +0530

    add new Controller by using add new item, After setup to Controller and Action

---
+++ ServiceStation.Web/Controllers/ServicesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceStation.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public String HelloWorld() {
+            return "This is a ServicesController";
+        } 
+    }
+
+    // MVC Handller,
+    //var servicesController = new ServicesController();
+    //servicesController.HelloWorld();
+}
